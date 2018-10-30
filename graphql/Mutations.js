2d1daff650f444b331a7dc93ab2e7efd86b199da
Katie Davis <katie.davis@shopify.com>
2018-10-30 10:29:48 -0400

    remove graphql stuff i do not need

--- graphql/Mutations.js
+++ graphql/Mutations.js
@@ -1,16 +1,5 @@
 import gql from 'graphql-tag';
 
-export const CREATE_PRODUCT = gql`
-  mutation CreateProduct($product: ProductInput!) {
-    productCreate(input: $product) {
-      product {
-        id
-        title
-      }
-    }
-  }
-`;
-
 export const UPDATE_PRODUCT = gql`
   mutation ProductUpdate($product: ProductInput!) {
     productUpdate(input: $product) {
--- graphql/Queries.js
+++ graphql/Queries.js
@@ -1,33 +1,5 @@
 import gql from 'graphql-tag';
 
-export const GET_PRODUCTS = gql`
-  query shop {
-    products(first: 250) {
-      edges {
-        node {
-          id
-          title
-        }
-      }
-      pageInfo {
-        hasNextPage
-      }
-    }
-  }
-`;
-
-export const GET_PRODUCT = gql`
-  query($handle: String!) {
-    shop {
-      productByHandle(handle: $handle) {
-        title
-        handle
-        bodyHtml
-      }
-    }
-  }
-`;
-
 export const GET_MANY_PRODUCTS = gql`
   query getproducts($id: [ID!]!) {
     nodes(ids: $id) {
